25e840dfab3375456ba9a2d81060c035957ea84e
Nistaux <nistaux@gmail.com>
2022-09-11 23:41:47 -0400

    figuring things out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::time::Duration;
+use enigo::{Enigo, Key, KeyboardControllable};
+
 fn main() {
-    println!("Hello, world!");
+    let mut t = 0;
+    while t < 8 {
+
+        let mut enigo = Enigo::new();
+
+        enigo.key_down(Key::Space);
+        enigo.key_up(Key::Space);
+        std::thread::sleep(Duration::from_secs(1));
+
+        t = t+1;
+    }
 }
